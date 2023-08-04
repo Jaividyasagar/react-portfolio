6aed88acb0afea32c5882dfbb30b49cb5087e38d
Jaividyasagar <88584859+Jaividyasagar@users.noreply.github.com>
2023-08-04 20:02:01 +0530

    Final Commit!

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import '../assets/css/swipe.css';
-import "./swipe.css";
-import '../assets/css/styles.css';
-import '../assets/css/bootstrap.min.css';
+import "./assets/css/swipe.css"
+import './assets/css/styles.css';
+import './assets/css/bootstrap.min.css';
 // import '../assets/css/swiper-bundle.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 const root = ReactDOM.createRoot(document.getElementById('root'));
