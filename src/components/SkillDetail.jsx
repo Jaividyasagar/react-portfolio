bec016006d406c310c8f1bfcedacc88a18f3e812
Jaividyasagar <88584859+Jaividyasagar@users.noreply.github.com>
2023-08-05 13:28:53 +0530

    Final Commit!

--- src/components/SkillDetail.jsx
+++ src/components/SkillDetail.jsx
@@ -18,12 +18,12 @@ const SkillDetail = ({ title, number, img }) => {
           {typeof number == "string" ? (
             <div
               className="skills__percentage"
-              style={{ width: `${number}` + "%" }}
+              style={{ width: `${number}%`}}
             ></div>
           ) : (
             <div
               className="skills__percentage"
-              style={{ width: "100" + "%" }}
+              style={{ width: "100%" }}
             ></div>
           )}
         </div>
