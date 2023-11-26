39fde3341aa1e0aa84d06a903105ebf91e0cceee
Jaividyasagar <88584859+Jaividyasagar@users.noreply.github.com>
2023-11-26 20:33:59 +0530

    Added Platform Developer Certification

--- src/assets/data/certificates.js
+++ src/assets/data/certificates.js
@@ -33,8 +33,10 @@ import first90Days from "../certificates/first-90-days.png";
 import effectiveListening from "../certificates/effective-listening.png";
 import excel from "../certificates/excel-essentials.png";
 import powerpoint from "../certificates/powerpoint-quick-tips.png";
+import platformdevelopercertified from "../certificates/Salesforce_PD1_Certified.png";
 
 export const certificates = [
+  platformdevelopercertified,
   reactRedux,
   sdlc,
   databases,
